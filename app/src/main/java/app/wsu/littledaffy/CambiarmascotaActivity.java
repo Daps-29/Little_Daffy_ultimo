9eea6f755da0661fbbbe875be15ab2e51bf54da3
Fernando Murguia <fmurguia12@gmail.com>
2024-01-17 15:25:43 -0400

    feat: :sparkles: ubicación de mascotas por separado

--- app/src/main/java/app/wsu/littledaffy/CambiarmascotaActivity.java
+++ app/src/main/java/app/wsu/littledaffy/CambiarmascotaActivity.java
@@ -1,21 +1,38 @@
 package app.wsu.littledaffy;
 
+import android.Manifest;
 import android.app.ProgressDialog;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.content.pm.PackageManager;
 import android.net.ConnectivityManager;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import app.wsu.littledaffy.databinding.ActivityMaps2Binding;
+import app.wsu.littledaffy.model.OrganizacionDto;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -24,6 +41,7 @@ import com.google.firebase.database.ValueEventListener;
 import com.squareup.picasso.Callback;
 import com.squareup.picasso.Picasso;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -31,18 +49,33 @@ import app.wsu.littledaffy.Utility.NetworkChangeListener;
 import app.wsu.littledaffy.model.MascotaDto;
 import de.hdodenhof.circleimageview.CircleImageView;
 
-public class CambiarmascotaActivity extends AppCompatActivity {
+import org.jetbrains.annotations.NotNull;
+
+public class CambiarmascotaActivity extends AppCompatActivity implements OnMapReadyCallback {
     CircleImageView imag;
-    TextView nombre,descripcion;
+    TextView nombre, descripcion;
     Spinner estado;
     Button guardar;
-    String idmascota,nombremascota,descripcionmascota;
+    String idmascota, nombremascota, descripcionmascota;
     DatabaseReference mascotainfo;
     ProgressDialog progressDialog;
+
+    // Configuracion para el mapa
+    private GoogleMap mMap;
+    LinearLayout containerMapa;
     NetworkChangeListener networkChangeListener = new NetworkChangeListener();
+
+    private DatabaseReference datosCollar;
+    private ArrayList<Marker> marke = new ArrayList<>();
+    private ArrayList<Marker> realmarkes = new ArrayList<>();
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        containerMapa = (LinearLayout) findViewById(R.id.containerMapa);
+
         setContentView(R.layout.activity_cambiarmascota);
         imag = findViewById(R.id.fotoestadp);
         nombre = findViewById(R.id.nombreestado);
@@ -50,8 +83,8 @@ public class CambiarmascotaActivity extends AppCompatActivity {
         estado = findViewById(R.id.Cambiar);
         guardar = findViewById(R.id.guardarestado);
 
-        String[]Estado={"Adoptado","Encontrado"};
-        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item,Estado);
+        String[] Estado = {"Adoptado", "Encontrado"};
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, R.layout.spinner_item_custom, Estado);
         estado.setAdapter(adapter);
 
         //OBTENER DATOS DE LA ANTERIOR ACTIVITY
@@ -92,8 +125,8 @@ public class CambiarmascotaActivity extends AppCompatActivity {
                 MascotaDto mascotaDto = new MascotaDto();
                 String estadomascota = estado.getSelectedItem().toString();
                 DatabaseReference base = FirebaseDatabase.getInstance().getReference("mascotas");
-                Map<String,Object> actualizar = new HashMap<>();
-                actualizar.put("estado",estadomascota);
+                Map<String, Object> actualizar = new HashMap<>();
+                actualizar.put("estado", estadomascota);
                 progressDialog = new ProgressDialog(CambiarmascotaActivity.this);
                 progressDialog.show();
                 progressDialog.setContentView(R.layout.progresdialog);
@@ -104,7 +137,93 @@ public class CambiarmascotaActivity extends AppCompatActivity {
             }
         });
 
+        // Datos para GOOGLE MAPS
+        try {
+            SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+            mapFragment.getMapAsync(this);
+//            actualizarUbicacion();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
     }
+
+    private void actualizarUbicacion() {
+        new CountDownTimer(10000, 1000) {
+            public void onTick(long millisUntilFished) {
+                Log.e("segundos", "" + millisUntilFished / 1000);
+            }
+
+            public void onFinish() {
+                ubicacionColar();
+            }
+        }.start();
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+
+        mMap = googleMap;
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+        mMap.setMyLocationEnabled(true);
+
+        FirebaseDatabase.getInstance().getReference("collares").orderByChild("mascota").equalTo(idmascota)
+                .addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot snapshot) {
+                        if (snapshot.exists()) {
+                            Toast.makeText(getApplicationContext(), "Mascota localizada", Toast.LENGTH_LONG).show();
+                            ubicacionColar();
+                        } else {
+                            Toast.makeText(getApplicationContext(), "La mascota no cuenta con un collar registrado", Toast.LENGTH_LONG).show();
+                            if (containerMapa != null) {
+                                containerMapa.setVisibility(View.INVISIBLE);
+                            }
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) {
+                        Toast.makeText(getApplicationContext(), "Error al buscar la ubicación de la mascota", Toast.LENGTH_LONG).show();
+                        if (containerMapa != null) {
+                            containerMapa.setVisibility(View.INVISIBLE);
+                        }
+                    }
+                });
+    }
+
+    private void ubicacionColar(){
+        datosCollar = FirebaseDatabase.getInstance().getReference("collar");
+        datosCollar.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                for (Marker marker:realmarkes){
+                    marker.remove();
+                }
+                for (DataSnapshot dataSnapshot:snapshot.getChildren()){
+                    Maps maps = dataSnapshot.getValue(Maps.class);
+                    Double latitud = maps.getF_latitude();
+                    Double longitud = maps.getF_longitude();
+                    MarkerOptions markerOptions = new MarkerOptions();
+                    LatLng sydney = new LatLng(latitud,longitud);
+                    markerOptions.position(new LatLng(latitud,longitud));
+                    mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+                    marke.add(mMap.addMarker(markerOptions));
+                }
+                realmarkes.clear();
+                realmarkes.addAll(marke);
+                actualizarUbicacion();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+    }
+
     @Override
     public void onStart() {
         IntentFilter filter = new IntentFilter(ConnectivityManager.CONNECTIVITY_ACTION);
@@ -118,4 +237,5 @@ public class CambiarmascotaActivity extends AppCompatActivity {
         unregisterReceiver(networkChangeListener);
         super.onStop();
     }
+
 }
